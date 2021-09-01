c00a5a1e24ec6199d3d9c4e1ae08195f428a589f
Andrea Lacava <thecave003@gmail.com>
2021-09-01 12:08:58 +0200

    add asn1c update

--- e2sim/src/ASN1c/TimeStamp.c
+++ e2sim/src/ASN1c/TimeStamp.c
@@ -22,7 +22,7 @@ TimeStamp_constraint(const asn_TYPE_descriptor_t *td, const void *sptr,
 	
 	size = st->size;
 	
-	if((size == 4)) {
+	if((size == 8)) {
 		/* Constraint check succeeded */
 		return 0;
 	} else {
@@ -39,10 +39,10 @@ TimeStamp_constraint(const asn_TYPE_descriptor_t *td, const void *sptr,
  */
 static asn_oer_constraints_t asn_OER_type_TimeStamp_constr_1 CC_NOTUSED = {
 	{ 0, 0 },
-	4	/* (SIZE(4..4)) */};
+	8	/* (SIZE(8..8)) */};
 asn_per_constraints_t asn_PER_type_TimeStamp_constr_1 CC_NOTUSED = {
 	{ APC_UNCONSTRAINED,	-1, -1,  0,  0 },
-	{ APC_CONSTRAINED,	 0,  0,  4,  4 }	/* (SIZE(4..4)) */,
+	{ APC_CONSTRAINED,	 0,  0,  8,  8 }	/* (SIZE(8..8)) */,
 	0, 0	/* No PER value map */
 };
 static const ber_tlv_tag_t asn_DEF_TimeStamp_tags_1[] = {
