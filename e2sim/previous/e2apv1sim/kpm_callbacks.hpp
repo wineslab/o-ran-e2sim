c7ac6a7304ceccf1818f9872b5b4f8ba792aefe9
Leonardo Bonati <leonardo.bonati@gmail.com>
2023-04-05 14:27:10 -0400

    Removed previous definitions

--- e2sim/previous/e2apv1sim/kpm_callbacks.hpp
+++
@@ -1,4 +0,0 @@
-
-void callback_kpm_subscription_request(E2AP_PDU_t *pdu, int socket_fd);
-
-void run_report_loop(long requestorId, long instanceId, long ranFunctionId, long actionId);
--- e2sim/previous/e2apv1sim/ricsim.hpp
+++
@@ -1,5 +0,0 @@
-#include "E2AP-PDU.h"
-
-void encode_and_send_sctp_data(E2AP_PDU_t* pdu);
-
-void encode_and_send_sctp_data(E2AP_PDU_t* pdu, int socket_fd);
